e2e2d1a17247743b5033e321b0867d715d0d1130
arkebi <rakesh8248@gmail.com>
2011-09-30 20:40:27 +0530

    Sample login prompt

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+ 		<title>CEA Fest Registration</title>
+	</head>
+
+	<body>
+		<!--post method used for security reasons-->
+		<form action="login.php" method="post">
+			Username: <input type="text" name="username"><br>
+			Password: <input name="password" type="password"><br>
+			<input value="Log in" type="submit">
+		</form>
+		Don't have an account? <a href="register.php">Register here.</a>
+	</body>
+</html>
